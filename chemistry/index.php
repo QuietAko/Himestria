eeddf69f385881e7df73cc4c2884c18bc0221c21
RetFiz <44443739+RetFiz@users.noreply.github.com>
2019-01-25 22:14:34 +0300

    Update index.php

--- chemistry/index.php
+++ chemistry/index.php
@@ -5,21 +5,10 @@
     ?>
 
     <div id="main">
-        <section>
+        
             <img id="header_icon" src="/chemistry/images/flask.png" width="32px" height="32px" name="flask" title="flask" />
-            <h1>Химия</h1>            
-        </section>
-        <section>
-            <h1>Учебные материалы</h1>
-            <ul>
-                <li><a href="/chemistry/library/stroenie-atoma.rtf">Строение атома (rtf)</a></li>
-                <li><a href="/chemistry/library/valentnost.doc">Валентность (doc)</a></li>
-                <li><a href="/chemistry/library/stepen-okisleniya.rtf">Степень окисления (rtf)</a></li>
-                <li><a href="/chemistry/library/himicheskaya-svyaz.rtf">Химическая связь (rtf)</a></li>
-                <li><a href="/chemistry/library/neorganicheskie-soedineniya.rtf">Неорганические соединения (rtf)</a></li>
-                <li><a href="/chemistry/library/tablitsa-reaktsiy.rtf">Таблица реакций (rtf)</a></li>
-            </ul>
-        </section>
+            <h1>Химия</h1>
+      
         <section>
             <h1>Упражнения</h1>
             <ul>
@@ -40,16 +29,6 @@
                 <!--<li><a href="chemistry/tasks/word_problem.php?type_id=3">Сложный уровень</a></li>-->
             </ul>
         </section>
-	<section>
-            <h1>Приложения</h1>
-            <ul>
-                <li><a href="/chemistry/apps/ted-v28.exe">Теория электролитической диссоциации (exe)</a></li>
-                <li><a href="/chemistry/apps/snake-v25.exe">Металлическая змейка (exe)</a></li>
-                <li><a href="/chemistry/apps/inorganic-compounds-v49.exe">Названия неорганических соединений (exe)</a></li>
-            </ul>
         </section>
-        <footer>
-            Касицкий Андрей Юрьевич
-        </footer>
     </div>
 </body>
